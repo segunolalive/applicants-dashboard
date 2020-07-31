c87bf315cfaca55a3ce96c9dc68956ea70efdaab
Segun Ola <segunolalive@gmail.com>
2020-07-31 06:34:18 +0100

    remove pending requests

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -7,7 +7,6 @@ const BASE_API_URL = process.env.REACT_APP_API_URL
 export function useApplicantData(searchText = '') {
   const [status, setStatus] = React.useState(REQUEST_STATUSES.IDLE)
   const [data, setData] = React.useState([])
-  const [pendingRequest, setPendingRequest] = React.useState(null as any)
   const abortRef: React.MutableRefObject<any> = React.useRef(null)
 
   const URL = searchText
@@ -25,7 +24,6 @@ export function useApplicantData(searchText = '') {
       setStatus(REQUEST_STATUSES.ERROR)
     } finally {
       setStatus(REQUEST_STATUSES.IDLE)
-      setPendingRequest(null)
     }
   }
 
@@ -36,10 +34,8 @@ export function useApplicantData(searchText = '') {
     const controller = new AbortController()
     abortRef.current = controller
 
-    setPendingRequest(URL)
     fetchApplicants(controller.signal)
     return () => {
-      setPendingRequest(null)
       controller.abort()
     }
   }, [searchText])
